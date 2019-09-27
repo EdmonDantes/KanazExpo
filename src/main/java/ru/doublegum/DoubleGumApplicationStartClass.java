a2c420fd0259f0d69811cac0074b0cd32770de30
Ilya Loginov <dantes2104@gmail.com>
2019-09-27 14:59:18 +0400

    Initial comment

---
+++ src/main/java/ru/doublegum/DoubleGumApplicationStartClass.java
@@ -0,0 +1,12 @@
+package ru.doublegum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleGumApplicationStartClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubleGumApplicationStartClass.class, args);
+    }
+}
