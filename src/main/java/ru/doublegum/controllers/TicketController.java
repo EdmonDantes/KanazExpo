6acf2998eb38cdf142d4dd66a1707ff9deed60d4
Kirill <lukashinkv@gmail.com>
2019-09-28 09:26:28 +0400

    Minor

--- src/main/java/ru/doublegum/controllers/TicketController.java
+++ src/main/java/ru/doublegum/controllers/TicketController.java
@@ -1,7 +1,10 @@
 package ru.doublegum.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.doublegum.HereApi;
+import ru.doublegum.entities.Ticket;
+import ru.doublegum.service.TicketService;
 
 import java.io.IOException;
 import java.util.Map;
@@ -10,10 +13,15 @@ import java.util.Map;
 @RequestMapping("/api/ticket")
 public class TicketController {
 
+    @Autowired
+    private TicketService ticketService;
+
     @RequestMapping(value = "/add", consumes = "application/json", method = RequestMethod.POST)
     public String add(@RequestBody double x, @RequestBody double y, @RequestBody String type){
         try {
             Map<String, String> address = HereApi.getAddressFromPoint(x, y);
+
+            //return ticketService.firstInsert()
 
         } catch (IOException e) {
             return "Wrong coordinates";
