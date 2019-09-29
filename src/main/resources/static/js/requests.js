b123bcf8838368e2aca932952e755af229659938
V1lgor <igor.volkov1999@gmail.com>
2019-09-29 09:11:39 +0300

    Revert "Added request page layout:"
    This reverts commit adc51c2fc4dae552975e62b35986fdb6a36196a0.
    
    # Conflicts:
    #	gradlew
    #	gradlew.bat
    #	src/main/resources/static/assets/css/style.css

---
+++ src/main/resources/static/js/requests.js
@@ -0,0 +1,51 @@
+let APILocation = "http://doublegum.site:8080";
+
+let loadProblemTypeSelect = async () => {
+    let response = await fetch("http://doublegum.site:8080/api/ticket/type/getAll");
+    console.log(response);
+
+    let categoriesList;
+
+    if (response.ok) {
+        categoriesList = await response.json();
+    } else {
+        console.log("HTTP error");
+    }
+
+    let problemList = document.getElementById("problemTypeSelect");
+
+    categoriesList.forEach((category) => {
+        let option = document.createElement("option");
+
+        option.innerText = category.name;
+        option.setAttribute('data-id', category.id);
+
+        problemList.appendChild(option);
+    });
+
+}
+
+let addProblemCategory = async () => {
+    let newTitle = document.getElementById("categoryTitle").value;
+    let newWeight = document.getElementById("categoryWeight").value;
+
+    let response = await fetch('http://doublegum.site:8080/api/ticket/type/add',
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json; charset="utf-8"'
+            },
+            body: JSON.stringify({name: newTitle, weight: newWeight})
+        });
+
+    let result = await response.json();
+
+    console.log(result);
+
+}
+
+let createTicket = () => {
+    let problemTypeSelect = document.getElementById("problemTypeSelect");
+    let problemTypeId = problemTypeSelect.options[problemTypeSelect.selectedIndex].getAttribute('data-id');
+    console.log("id:" + problemTypeId);
+}
